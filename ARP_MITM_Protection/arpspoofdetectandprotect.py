7886adcf8f1fbd96ee371d6f5a4946ac7a36a00b
Denis <87089625+DenisMarcher@users.noreply.github.com>
2025-11-16 20:35:36 +0200

    Add files via upload

---
+++ ARP_MITM_Protection/arpspoofdetectandprotect.py
@@ -0,0 +1,35 @@
+from scapy.all import *
+from scapy.layers.l2 import ARP
+import os
+
+
+
+def restore_arp(ip, mac) -> None:
+	print(f"[INFO] Restoring ARP Entry for {ip} -> {mac}")
+	os.system(f"arp -s {ip} {mac}")
+
+def detect_arp_spoof(packet, ip, mac) -> None:
+	if packet.haslayer(ARP) and packet[ARP].op == 2:
+		sender_ip = packet[ARP].psrc
+		sender_mac = packet[ARP].hwsrc
+
+		if sender_ip == ip and sender_mac != mac:
+			print(f"[ALERT] Detected ARP Spoofing! Router IP ({ip}) is being spoofed by {sender_mac}")
+			restore_arp(ip, mac)
+
+
+
+def monitor_arp(interface) -> None:
+	print("[INFO] Starting ARP Spoofing Detection ... on" + interface)
+	restore_arp(ROUTER_IP, ROUTER_MAC)
+	sniff(iface=interface, filter="arp", store=0, prn=detect_arp_spoof)
+
+
+if __name__ == "__main__":
+	ROUTER_IP = ""
+	ROUTER_MAC = ""
+	interface = "eth0"
+	try:
+		monitor_arp(interface)
+	except KeyboardInterrupt:
+		print("[INFO] Stopping ARP Spoof Protection script.")
